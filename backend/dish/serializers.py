a673ba50494f111f032e0dedcaf435ef20bd1567
Oleksandra <mremarka314@gmail.com>
2023-10-29 19:18:36 +0200

    add basic serializers

--- backend/dish/serializers.py
+++ backend/dish/serializers.py
@@ -1,8 +1,26 @@
 from rest_framework.serializers import ModelSerializer
-from .models import Product
+from .models import Product, Ingredient, Instruction, Receipt
 
 
 class ProductSerializer(ModelSerializer):
     class Meta:
         model = Product
+        fields = "__all__"
+
+
+class IngredientSerializer(ModelSerializer):
+    class Meta:
+        model = Ingredient
+        fields = "__all__"
+
+
+class InstructionSerializer(ModelSerializer):
+    class Meta:
+        model = Instruction
+        fields = "__all__"
+
+
+class ReceiptSerializer(ModelSerializer):
+    class Meta:
+        model = Receipt
         fields = "__all__"
